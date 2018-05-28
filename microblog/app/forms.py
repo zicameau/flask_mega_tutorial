d563f6d356a8f1551f870efdff625a595b719f75
zicameau <rowdeil@gmail.com>
2018-05-28 10:36:28 -0700

    Added the last_seen functionality, which shows when the last time the user has been on the server and the about_me functionality which allows the user to describe themselves on their profile.

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -8,6 +8,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')    
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
